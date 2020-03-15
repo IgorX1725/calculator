1c2fd34d31d019ef1edbc58e9a340e06fbcfa186
Igor Cortez <igor.barbosa14@gmail.com>
2020-03-15 00:01:50 -0300

    calculator logic implemented

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,21 +1,32 @@
+/* eslint no-eval: 0 */
 import React, {Component} from 'react'
 import './Calculator.css'
 import Button from '../components/Button'
 import Display from '../components/Display'
 
+const initialState ={
+    displayValue: '0',
+    clearMemory : false,
+    operation : null,
+    values : [0,0],
+    current : 0
+}
+
 export default class Calculator extends Component{
+
+    state = {...initialState}
 
     constructor(props){
         super(props)
-        this.clearDisplay = this.clearDisplay.bind(this)
+        this.clearMemory = this.clearMemory.bind(this)
         this.addDigit = this.addDigit.bind(this)
         this.setOperation = this.setOperation.bind(this)
     }
     render(){
         return (
             <div className="calculator">
-                <Display value="0" ></Display>
-                <Button label="AC" click={this.clearDisplay} triple />
+                <Display value={this.state.displayValue} ></Display>
+                <Button label="AC" click={this.clearMemory} triple />
                 <Button label="/" click={this.setOperation} operation/>
                 <Button label="7" click={this.addDigit} />
                 <Button label="8" click={this.addDigit} />
@@ -37,12 +48,52 @@ export default class Calculator extends Component{
         )
     }
     setOperation(operation){
-        console.log(operation)
+        if(this.state.current === 0){
+            this.setState({operation, current : 1, clearDisplay:true})
+        }else{
+            const equals = operation === '=' 
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+            try{
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+            }catch(e){
+                values[0] = this.state.values[0]
+            }
+            values[1] = 0
+
+            this.setState({
+                displayValue : values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+        }
     }
-    clearDisplay(){
-        console.log("limpar")
+    clearMemory(){
+        this.setState({...initialState})
     }
     addDigit(digit){
-        console.log(digit)
+        if(digit === '.' && this.state.displayValue.includes('.') ){
+            return
+        }else if(this.state.displayValue.length === 10){
+            return
+        }
+        const clearDisplay = this.state.displayValue === '0'
+        || this.state.clearDisplay
+
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+
+        const displayValue = currentValue + digit
+        this.setState({displayValue,clearDisplay:false})
+        if(digit !== '.'){
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({values})
+            console.log(values)
+        }
     }
 }
